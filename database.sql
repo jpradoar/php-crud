094228dd1a0080c7a7fe41584e20e9b03cd6490e
Jonathan Prado <jpradoar@gmail.com>
2019-04-29 00:52:24 +0000

    first commit

---
+++ database.sql
@@ -0,0 +1,16 @@
+create database test;
+
+use test;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) NOT NULL,
+  `data1` varchar(20) NULL,
+  `data2` varchar(20) NULL,
+  `data3` varchar(20) NULL,
+  `data4` varchar(20) NULL,
+  `data5` varchar(20) NULL,
+  `data6` varchar(20) NULL,
+  `data7` varchar(20) NULL,
+  PRIMARY KEY  (`id`)
+);
