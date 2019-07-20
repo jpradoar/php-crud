d0228c640d17a0c077116b73888c1bb4e3a66656
Jonathan <jpradoar@gmail.com>
2019-07-20 19:24:25 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -2,15 +2,21 @@ create database test;
 
 use test;
 
+DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `data1` varchar(20) NULL,
-  `data2` varchar(20) NULL,
-  `data3` varchar(20) NULL,
-  `data4` varchar(20) NULL,
-  `data5` varchar(20) NULL,
-  `data6` varchar(20) NULL,
-  `data7` varchar(20) NULL,
-  PRIMARY KEY  (`id`)
-);
+  `data1` varchar(20) DEFAULT NULL,
+  `data2` varchar(20) DEFAULT NULL,
+  `data3` varchar(20) DEFAULT NULL,
+  `data4` varchar(20) DEFAULT NULL,
+  `data5` varchar(20) DEFAULT NULL,
+  `data6` varchar(20) DEFAULT NULL,
+  `data7` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `users` WRITE;
+INSERT INTO `users` VALUES (1,'test01','test01',NULL,NULL,NULL,NULL,NULL,NULL),(2,'test02','test02',NULL,NULL,NULL,NULL,NULL,NULL),(3,'test03','test03',NULL,NULL,NULL,NULL,NULL,NULL),(4,'test04','test04',NULL,NULL,NULL,NULL,NULL,NULL),(5,'test05','test05',NULL,NULL,NULL,NULL,NULL,NULL);
+UNLOCK TABLES;
